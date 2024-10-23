07291d179c5c4a0b2a5c7f46d466e2ca41659e00
tallestgabs <gabrielcastrodias1414@gmail.com>
2024-10-23 13:04:45 -0300

    folders created

---
+++ springBoot/src/main/java/com/tallestgabs/horseRun/HorseRunApplication.java
@@ -0,0 +1,13 @@
+package com.tallestgabs.horseRun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseRunApplication.class, args);
+	}
+
+}
---
+++ springBoot/src/test/java/com/tallestgabs/horseRun/HorseRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallestgabs.horseRun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
